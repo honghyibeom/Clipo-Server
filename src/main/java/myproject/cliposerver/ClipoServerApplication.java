41a9eb78b02b0e29983f1cdc9d5482595aa57e25
honghyibeom <ghdgmlqja1@naver.com>
2024-07-12 14:55:53 +0900

    init

---
+++ src/main/java/myproject/cliposerver/ClipoServerApplication.java
@@ -0,0 +1,13 @@
+package myproject.cliposerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipoServerApplication.class, args);
+    }
+
+}
