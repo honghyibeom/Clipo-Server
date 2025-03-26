5a0a1da7014a9de6f512181a297d307fb8c64a80
honghyibeom <ghdgmlqja1@naver.com>
2025-03-26 17:17:25 +0900

    검색 기능 초안

--- src/main/java/myproject/cliposerver/service/member/MemberService.java
+++ src/main/java/myproject/cliposerver/service/member/MemberService.java
@@ -3,10 +3,8 @@ package myproject.cliposerver.service.member;
 import myproject.cliposerver.config.security.UserDetailsImpl;
 import myproject.cliposerver.data.dto.ResponseDTO;
 import myproject.cliposerver.data.dto.member.UpdatePasswordRequestDTO;
-import myproject.cliposerver.data.dto.member.UpdateProfileNicknameRequestDTO;
 import myproject.cliposerver.data.dto.member.UpdateUserInfoRequestDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
