a6ac26ae49c12dff6ca96b294f052a607bbe908d
honghyibeom <ghdgmlqja1@naver.com>
2025-07-21 15:23:00 +0900

    일단 커밋

--- src/main/java/myproject/cliposerver/exception/CustomExceptionHandler.java
+++ src/main/java/myproject/cliposerver/exception/CustomExceptionHandler.java
@@ -5,13 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice // 전역 예외 처리기
 @Slf4j
 public class CustomExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> handleApiRequestException(CustomException customException) {
         log.error(customException.getErrorCode().getErrorMessage());
-        return ResponseEntity.status(customException.getErrorCode().getHttpStatus()).body(new CustomExceptionDTO(customException.getErrorCode()));
+        return ResponseEntity.
+                status(customException.getErrorCode().getHttpStatus())
+                .body(new CustomExceptionDTO(customException.getErrorCode()));
     }
 }
