aa81c413e35bc9e1730ba714e2a8cf9888d89e4d
honghyibeom <ghdgmlqja1@naver.com>
2025-09-17 14:39:29 +0900

    수정

--- src/main/java/myproject/cliposerver/config/security/CorsConfig.java
+++ src/main/java/myproject/cliposerver/config/security/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://clipofront.netlify.app/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
